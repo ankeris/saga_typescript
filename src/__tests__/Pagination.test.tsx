d0632597a815d106166ddccf68d400f90735f8de
Juozas Rastenis <juozas.rastenis@gmail.com>
2020-02-17 23:46:52 +0100

    feat(app): Pagination Emit ev. unit test added

--- src/__tests__/Pagination.test.tsx
+++ src/__tests__/Pagination.test.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { render, wait, fireEvent } from "@testing-library/react";
 import Pagination from "@/components/Pagination";
 
-const renderPagination = () => render(
-    <Pagination onPageChange={() => { }} />
-);
-
 describe("<Pagination />", () => {
 
     test("Pressing button Increments/Decrements the page", () => {
-        const { getByText } = renderPagination();
+        const { getByText } = render(<Pagination onPageChange={() => { }} />);
         expect(getByText("Page: 1")).toBeInTheDocument();
         fireEvent.click(getByText(">"));
         expect(getByText("Page: 2")).toBeInTheDocument();
         fireEvent.click(getByText("<"));
         expect(getByText("Page: 1")).toBeInTheDocument();
+    });
+
+    test("Pagination emits right value", async () => {
+        const timesPressed = Math.round(Math.random() * 10);
+        let lastOnChangeValueEmitted: number = 0;
+        const { getByText } = render(<Pagination onPageChange={x => lastOnChangeValueEmitted = x} />);
+        for (let i = 1; i < timesPressed; i++) {
+            fireEvent.click(getByText(">"));
+        }
+        expect(lastOnChangeValueEmitted).toBe(timesPressed)
     });
 
 });
